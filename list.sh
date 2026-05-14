041405f70793a47a4c94d59fd06bb8fd8bcd5fd7
mouadm0uad <mouadbus2022@gmail.com>
2026-05-14 17:35:47 +0000

    Ajout des scripts de la semaine 03

---
+++ list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Analyse de la taille des dossiers dans /home :"
+echo "--------------------------------------------"
+
+sudo du -sh /home/*
+
+echo "--------------------------------------------"
+echo "Analyse terminée."
---
+++ script_users.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Mot de passe par défaut
+PASSWORD="Pass123!"
+
+for i in {1..5}
+do
+    USERNAME="user$i"
+    # Création de l'utilisateur avec répertoire home et mot de passe chiffré
+    sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
+    echo "Utilisateur $USERNAME créé avec succès."
+done
